84ddcfb41fa0f07e05278bbf647ed44c2ba505a5
Diego Gaspar <cadastro@diegogaspar.dev.br>
2024-01-06 20:59:04 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"github.com/gaspartv/API-GO-opportunities/config"
 	"github.com/gaspartv/API-GO-opportunities/router"
 )
 
+var (
+	logger config.Logger
+)
+
 func main() {
+    logger = *config.GetLogger("main")
+
+	// Initialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.ErrorF("config initialization error: %v", err)
+        return
+	}
+
+	// Initialize Router
 	router.Initialize()
 }
