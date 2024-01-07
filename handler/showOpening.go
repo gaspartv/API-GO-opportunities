b79f9173ec449989de89288e8ca9585e35a1d603
Diego Gaspar <cadastro@diegogaspar.dev.br>
2024-01-06 23:47:38 -0300

    docs: add documentation for Show Openings

--- handler/showOpening.go
+++ handler/showOpening.go
@@ -7,7 +7,18 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// @BasePath /api/v1
 
+// @Summary Show opening
+// @Description Show a new job opening
+// @Tags Openings
+// @Accept json
+// @Produce json
+// @Param id query string true "Opening identification"
+// @Success 200 {object} ShowOpeningResponse
+// @Failure 404 {object} ErrorResponse
+// @Failure 500 {object} ErrorResponse
+// @Router /opening [get]
 func ShowOpeningHandler(ctx *gin.Context){
 	id := ctx.Query("id")
 	if id == "" {
